b55fc5448dea008835516772b029e4cc34965384
danisg97 <sanchez.gracia.daniel@gmail.com>
2019-09-10 22:40:29 +0200

    React - Functional Component

---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Functional Component.
+
+// Cuerpo del componente. Arrow function con codigo JSX.
+const WeatherLocation = () => (
+    <div> Weather Location </div>
+);
+
+// Exportamos por defecto.
+export default WeatherLocation;
